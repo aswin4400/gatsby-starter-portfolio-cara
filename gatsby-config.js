054cb9096e15fe8994075ef05693a6a99e319df2
aswin4400 <aswin4400@gmail.com>
2020-08-23 00:16:49 +0530

    new icons have been added since it was'nt working

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Aswin Rajasekharan`,
+        short_name: `Aswin`,
+        description: `A Simple & Humble Developer`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
